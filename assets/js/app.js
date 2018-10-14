ce2d6d226242b008809b853329992d21c45440b5
Conrado Levy <conrado.lk@gmail.com>
2018-10-14 02:15:01 -0300

    * Agregado login funcional

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,6 +8,7 @@
 // any CSS you require will output into a single css file (app.scss in this case)
 require('../css/app.scss');
 require('../css/navigation.scss');
+require('../css/login.scss');
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 var $ = require('jquery');
