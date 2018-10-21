523c6fe127b94f8925ae7e6447e565cf888e5919
Emanuel <emanuelbastons@gmail.com>
2018-10-21 12:09:32 -0300

    Added account configuration

---
+++ src/Controller/AccountConfigurationController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\AccountConfigurationType;
+use App\Entity\Client;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Doctrine\DBAL\Exception\UniqueConstraintViolationException;
+use Doctrine\DBAL\DBALException;
+
+
+class AccountConfigurationController extends AbstractController
+{
+    /**
+     * @Route("/account configuration", name="account_configuration")
+     */
+    public function configure(Request $request, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        // 1) build the form
+        $client = $this->get('security.token_storage')->getToken()->getUser(); 
+        $form = $this->createForm(AccountConfigurationType::class, $client);
+        $error = false;
+        $errorMessage = '';
+
+        // 2) handle the submit (will only happen on POST)
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            try {
+                // 3) Encode the password (you could also do this via Doctrine listener)
+                $password = $passwordEncoder->encodePassword($client, $client->getPassword());
+                $client->setPassword($password);
+
+                // 4) save the User!
+                $entityManager = $this->getDoctrine()->getManager();
+                $entityManager->persist($client);
+                $entityManager->flush();
+
+            } catch (UniqueConstraintViolationException $e) {
+                $error = true;
+                $errorMessage = $e->getMessage();
+            } catch (DBALException $e) {
+                $error = true;
+                $errorMessage = 'Se produjo un error al intentar actualizar los datos';
+            }
+
+            if ($error) {
+                return $this->render(
+                    'account_configuration/index.html.twig', //Llama a la vista 
+                    array(
+                        'form' => $form->createView(),
+                        'error' => $errorMessage)
+                );
+            } else {
+                $this->addFlash(
+                    'success',
+                    'Los datos de tu cuenta han sido actualizados correctamente.'
+                );
+                return $this->redirectToRoute('home'); //Colocar a donde redireccionar
+            }
+        }
+
+        return $this->render(
+            'account_configuration/index.html.twig',
+            array(
+                'form' => $form->createView(),
+                'error' => $errorMessage)
+        );
+    }
+}
---
+++ src/Form/AccountConfigurationType.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Client;
+use App\Entity\SecretQuestion;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+
+class AccountConfigurationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email', EmailType::class, array(
+                'attr' => array(
+                    'data-parsley-trigger' => "focusout",
+                    'group' => 'block1',
+                    'data-parsley-errors-container' => '#email-input-errors',
+                    'maxlength'=>'50',
+                ),
+                'error_bubbling' => true,
+            ))
+
+            ->add('first_name', TextType::class, array(
+                'label' => 'Nombre',
+                'attr' => array(
+                    'data-parsley-trigger' => "focusout",
+                    'data-parsley-errors-container' => '#first-name-input-errors',
+                    'data-parsley-length' => '[2, 24]',
+                    'maxlength'=>'24',
+                    'group' => 'block1',
+                    'autofocus' => '')
+            ) )
+            ->add('last_name', TextType::class, array(
+                'label' => 'Apellido',
+                'attr' => array(
+                    'data-parsley-trigger' => "focusout",
+                    'group' => 'block1',
+                    'data-parsley-errors-container' => '#last-name-input-errors',
+                    'data-parsley-length' => '[2, 24]',
+                    'maxlength'=>'24',
+                )
+            ))
+
+
+
+
+            ;
+
+
+
+           /* De momento el usuario no puede cambiar su pass
+            ->add('password', RepeatedType::class, array(
+                'type' => PasswordType::class,
+                'first_options' => array(
+                    'label' => 'Contraseña',
+                    'help' => 'Debe tener entre 4 y 24 caracteres alfanumericos',
+                    'attr' => array(
+                        'data-parsley-trigger' => "focusout",
+                        'group' => 'block1',
+                        'data-parsley-errors-container' => '#first-password-input-errors',
+                        'data-parsley-length' => '[4, 24]',
+                        'maxlength'=>'24',
+                    )),
+                'second_options' => array(
+                    'label' => 'Repita la contraseña',
+                    'help' => 'Debe coincidir con la contraseña',
+                    'attr' => array(
+                        'data-parsley-trigger' => "focusout",
+                        'group' => 'block1',
+                        'data-parsley-errors-container' => '#second-password-input-errors',
+                        'data-parsley-length' => '[4, 24]',
+                        'maxlength'=>'24',
+                        'data-parsley-equalto' => "#client_password_first",
+                        'data-parsley-equalto-message' => 'La contraseña debe concidir.')),
+                'attr' => array(
+                    'data-parsley-trigger' => "focusout",
+                    'group' => 'block1')
+            )
+        )*/
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => Client::class,
+        ));
+    }
+}
