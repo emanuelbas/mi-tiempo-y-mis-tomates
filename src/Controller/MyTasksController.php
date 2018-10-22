b734311b78cf3bbed9871bd9ac25ce1ac717995c
Conrado Levy <conrado.lk@gmail.com>
2018-10-21 21:29:45 -0300

    * Agregado filtro persistente ante cambio de URL * Agregando paginacion

--- src/Controller/MyTasksController.php
+++ src/Controller/MyTasksController.php
@@ -12,14 +12,14 @@ use Symfony\Component\HttpFoundation\Request;
 class MyTasksController extends AbstractController
 {
     /**
-     * @Route("/my-tasks/{page}/{state}", name="my_tasks",defaults={"page"=1, "state"=0})
+     * @Route("/my-tasks/{page}/{state}/{sort}", name="my_tasks",defaults={"page"=1, "state"=0, "sort"=0})
      */
-    public function index(Request $request, $page, $state)
+    public function index(Request $request, $page, $state, $sort)
     {
         $clientId = $this->get('security.token_storage')->getToken()->getUser()->getId();
         $entityManager = $this->getDoctrine()->getManager();
         if ($state == 0) { // Ver todos
-            $tasks = $entityManager->getRepository("App\Entity\Task")->findBy(['client' => $clientId]);
+            $tasks = $entityManager->getRepository("App\Entity\Task")->findBy(['client' => $clientId],[],2,$page);
         } else { // Pendiente o finalizado
             $tasks = $entityManager->getRepository("App\Entity\Task")->findBy(['task_state' => $state, 'client' => $clientId]);
         }
