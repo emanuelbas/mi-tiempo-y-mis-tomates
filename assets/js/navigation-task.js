48153433fc5c63a2b5750311ba2f52fd4ea68f33
Conrado <conrado.lk@gmail.com>
2018-11-21 19:47:19 -0300

    * Migrada visualización de reloj a la librería countdownTimer * Actualizada librería countdownTimer

--- assets/js/navigation-task.js
+++ assets/js/navigation-task.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
    // Usar código de cuenta regresiva y refresco aca
+    if (inicio === undefined) {
+        return;
+    }
+
+   $("#js-clock").countdowntimer({
+    startDate: new Date().getTime(),
+    dateAndTime: inicio,
+    displayFormat: 'MS',
+    timeUp: timerFinish
+  });
+
+  function timerFinish() {
+    alert ("Terminó el periodo de" + periodType);
+    window.location.href = refreshRoute;
+   }
 });
