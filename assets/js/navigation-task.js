16c9e1e70753c53a03ef1a34b8ccc55a8c4856a5
Conrado <conrado.lk@gmail.com>
2018-11-21 22:36:53 -0300

    * Agregado chequeo de configuracion de sonidos * Agregada funcion playLoopedSound en la libreria * Arreglados errores de JS * Agregado ticktock loopeable

--- assets/js/navigation-task.js
+++ assets/js/navigation-task.js
@@ -1,11 +1,29 @@
 $(document).ready(function(){
 
+   if (window.localStorage.getItem('notifyNewPomodoro') === "true" ) {
+    notifyNewPomodoro();
+    window.localStorage.setItem("notifyNewPomodoro", false);
+  } 
+
+  function notifyNewPomodoro () {
+    $('#button-next-action').popover('show')
+    $.playSound("/sound/clock/alarm.mp3");   
+  }
+
+  if ( !$("#js-clock").length ) {
+     return;
+  }
+
    $("#js-clock").countdowntimer({
     startDate: new Date().getTime(),
     dateAndTime: inicio,
     displayFormat: 'MS',
     timeUp: timerFinish
   });
+
+   if (tickAlarm) {
+    $.playLoopedSound("/sound/clock/tick_tock.mp3");   
+   }
 
   $('.popover-dismiss').popover({
     trigger: 'focus'
@@ -19,11 +37,5 @@ $(document).ready(function(){
   function timerFinish() {
     window.localStorage.setItem("notifyNewPomodoro", "true");
     window.location.href = refreshRoute;
-   }
-
-  function notifyNewPomodoro () {
-    console.log('notify');
-    $('#button-next-action').popover('show')
-    $.playSound("/sound/clock/alarm.mp3");   
-  }
+  } 
 });
--- assets/js/plugins/jquery.playSound.js
+++ assets/js/plugins/jquery.playSound.js
@@ -18,6 +18,14 @@
                      + '<embed src="' + arguments[0] + '" hidden="true" autostart="true" loop="false"/>'
                    + '</audio>'
                  ).appendTo('body');
+        },        
+        playLoopedSound: function () {
+            return $(
+                   '<audio class="sound-player" autoplay="autoplay" loop style="display:none;">'
+                     + '<source src="' + arguments[0] + '" />'
+                     + '<embed src="' + arguments[0] + '" hidden="true" autostart="true" loop="false"/>'
+                   + '</audio>'
+                 ).appendTo('body');
         },
         stopSound: function () {
             $(".sound-player").remove();
