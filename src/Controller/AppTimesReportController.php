a4850619a8a21264127871d3c82e26b3dca22f19
Conrado <conrado.lk@gmail.com>
2018-12-01 22:11:10 -0300

    * Agregando lógica de filtrado por periodo

--- src/Controller/AppTimesReportController.php
+++ src/Controller/AppTimesReportController.php
@@ -16,10 +16,21 @@ class AppTimesReportController extends AbstractController
     public function index($period)
     {
         $entityManager = $this->getDoctrine()->getManager();
-
         $clientId = $this->get('security.token_storage')->getToken()->getUser()->getId();
         $finishedStateId = $entityManager->getRepository("App\Entity\TaskState")
             ->findBy(["state" => "FINISHED"]);
+
+        switch ($period) {
+            case "week":
+                $orderBy['task_name'] = "ASC";
+                break;
+            case "month":
+                $orderBy['creation_date'] = "DESC";
+                break;
+            case "year":
+                $orderBy['stimated_pomodoros'] = "DESC";
+                break;
+        }
 
         $tasks = $entityManager->getRepository("App\Entity\Task")
             ->findBy(["client" => $clientId, "task_state" => $finishedStateId]);
