2652e363dc9fa9e703e76717cf33449a49b71a47
Conrado <conrado.lk@gmail.com>
2018-12-05 02:48:15 -0300

    * Agregado envío de email funcional al comando programado

--- src/Command/SendEmail.php
+++ src/Command/SendEmail.php
@@ -6,10 +6,24 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use App\Entity\Client;
+use Doctrine\ORM\EntityManagerInterface;
 
 
 class SendEmail extends Command
 {
+
+    private $mailer;
+    private $entityManager;
+
+    public function __construct(\Swift_Mailer $mailer, EntityManagerInterface $entityManager)
+    {
+        $this->mailer = $mailer;
+        $this->entityManager = $entityManager;
+
+        // you *must* call the parent constructor
+        parent::__construct();
+    }
+
     // the name of the command (the part after "bin/console")
     protected static $defaultName = 'app:send-email';
 
@@ -18,33 +32,38 @@ class SendEmail extends Command
         $this
             ->setDescription('Envia un mail')
 
-            ->setHelp('Envia un mail a un usuario dependiendo de su configuracion')
-        ;
+            ->setHelp('Envia un mail a un usuario dependiendo de su configuracion');
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output)
-    {
-        // ...
-        $entityManager = $this->getDoctrine()->getManager();
+    private function sendEmail ($email) {
+        $message = (new \Swift_Message('Reporte de Mi Tiempo y Mis Tomates'))
+        ->setFrom('no-reply@mitiempoymistomates.com')
+        ->setTo($email)
+        ->setBody('- Email de reporte de prueba -');
 
-        $clients =$entityManager->getRepository("App\Entity\Client")->findAll();
+        $this->mailer->send($message);
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output)
+    {    
+        $clients = $this->entityManager->getRepository("App\Entity\Client")->findAll();
         foreach ($clients as $client){
             $config = $client->getReportFrequency();
-            $day= getdate();
-            if($config=="mensual"){
-                if($day['mday']== 1){
-                    //SENDMAIL
-                }
-            }elseif ($config=="anual"){
-                if($day['mon']== 1){
-                //SENDMAIL
-            }
+            $day = getdate();
 
-            }else{
-                //sendmail
+            if($config == "Mensual") {
+                if($day['mday'] == 1) {
+                    $this->sendEmail($client->getEmail());
+                }
+            } elseif ($config=="Anual"){
+                if($day['mon'] == 1) {
+                    $this->sendEmail($client->getEmail());
+                }
+            } else {
+                $this->sendEmail($client->getEmail());
             }
         }
 
-        $output->writeln('User successfully generated!');
+        $output->writeln('Enviando emails de reporte..');
     }
 }
